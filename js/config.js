48bddab080fec0b37fb67710284756c45b73a353
f83324222-cmyk <f83324222@gmail.com>
2026-08-31 21:02:17 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,2 @@
-window.APROVAQUEST_CONFIG = {
-  SUPABASE_URL: "https://xmhuxjgsgsivqlkzrvml.supabase.co/rest/v1/",
-  SUPABASE_ANON_KEY: "sb_publishable_4cxqEDZD5ENc66pZEtinJA_8yNKBshm"
-};
+export const SUPABASE_URL = "https://xmhuxjgsgsivqlkzrvml.supabase.co/rest/v1/";
+export const SUPABASE_ANON_KEY = "sb_publishable_4cxqEDZD5ENc66pZEtinJA_8yNKBshm";
